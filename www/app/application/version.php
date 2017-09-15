04bda04932fc7d63cba355ddfcbf31e7a89b2ed6
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2017-09-15 12:18:43 -0500

    working cal styling

--- www/app/application/version.php
+++ www/app/application/version.php
@@ -1,3 +1,3 @@
 <?
-define("APPVERSION", "1505452995957");
+define("APPVERSION", "1505495907360");
 ?>
--- www/app/application/views/partials/dashboard/events.php
+++ www/app/application/views/partials/dashboard/events.php
@@ -46,28 +46,28 @@
                     <% } %>
                     </div>
                      <!-- 2. Include script -->
-    <script type="text/javascript">(function () {
-            if (window.addtocalendar)if(typeof window.addtocalendar.start == "function")return;
-            if (window.ifaddtocalendar == undefined) { window.ifaddtocalendar = 1;
-                var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
-                s.type = 'text/javascript';s.charset = 'UTF-8';s.async = true;
-                s.src = ('https:' == window.location.protocol ? 'https' : 'http')+'://addtocalendar.com/atc/1.5/atc.min.js';
-                var h = d[g]('body')[0];h.appendChild(s); }})();
-    </script>
+                    <script type="text/javascript">(function () {
+                            if (window.addtocalendar)if(typeof window.addtocalendar.start == "function")return;
+                            if (window.ifaddtocalendar == undefined) { window.ifaddtocalendar = 1;
+                                var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
+                                s.type = 'text/javascript';s.charset = 'UTF-8';s.async = true;
+                                s.src = ('https:' == window.location.protocol ? 'https' : 'http')+'://addtocalendar.com/atc/1.5/atc.min.js';
+                                var h = d[g]('body')[0];h.appendChild(s); }})();
+                    </script>
 
-    <!-- 3. Place event data -->
-    <span class="addtocalendar atc-base">
-        <var class="atc_event">
-            <var class="atc_date_start"><%= event.date %> <%= event.start_time %></var>
-            <var class="atc_date_end"><%= event.date %> <%= event.end_time %></var>
-            <var class="atc_timezone"><%= app.user.timezone %></var>
-            <var class="atc_title"><%= event.title %></var>
-            <var class="atc_description"><%= event.text %></var>
-            <var class="atc_location">Video Session</var>
-            <var class="atc_organizer"><%= app.user.counselor %></var>
-            <var class="atc_organizer_email">luke@starwars.com</var>
-        </var>
-    </span>
+                    <!-- 3. Place event data -->
+                    <span class="addtocalendar atc-base">
+                        <var class="atc_event">
+                            <var class="atc_date_start"><%= event.date %> <%= event.start_time %></var>
+                            <var class="atc_date_end"><%= event.date %> <%= event.end_time %></var>
+                            <var class="atc_timezone"><%= app.user.timezone %></var>
+                            <var class="atc_title"><%= event.title %></var>
+                            <var class="atc_description">Video Session with <%= app.user.counselor %></var>
+                            <var class="atc_location">Video Session</var>
+                            <var class="atc_organizer"><%= app.user.counselor %></var>
+                            <var class="atc_organizer_email">luke@starwars.com</var>
+                        </var>
+                    </span>
                 </div>
             </div>
         </div>
