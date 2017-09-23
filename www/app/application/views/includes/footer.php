4808a1ad95f61e0234ef8dd065e480837c98f020
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2017-09-23 00:18:32 -0500

    adding intercom

--- www/app/application/views/includes/footer.php
+++ www/app/application/views/includes/footer.php
@@ -32,7 +32,15 @@
                     </script>
 <script>
   window.intercomSettings = {
-    app_id: "w7qc2x21"
+    app_id: "w7qc2x21",
+    user_id: "<?php echo $current_user->id ?>", // User ID
+    user_hash: "<?php
+      echo hash_hmac(
+        'sha256',
+        $user->id,
+        '3qenI3IiDbaxJr5mvLLJA72uASR83w9kTpuhOD4N'
+      );
+    ?>" // HMAC using SHA-256
   };
 </script>
 <script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/w7qc2x21';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
