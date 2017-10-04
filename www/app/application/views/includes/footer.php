ff2b3beb9e1b8a63d1226f7e15725742adf27c86
Gabriel Centenaro <gabrielcentenaro90@yahoo.com>
2017-10-04 10:51:57 -0500

    readding calendar function for events

--- www/app/application/views/includes/footer.php
+++ www/app/application/views/includes/footer.php
@@ -22,16 +22,5 @@
 <script src="/app/partials/all" type="text/javascript"></script>
 <script src="https://js.stripe.com/v2/" type="text/javascript"></script>
 <script type="text/javascript">Stripe.setPublishableKey('<?=$this->config->item('stripe_public_key')?>');</script>
-<script type="text/javascript">(function () {
-  if (window.addtocalendar)if(typeof window.addtocalendar.start == "function")return;
-  if (window.ifaddtocalendar == undefined) { window.ifaddtocalendar = 1;
-      var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
-      s.type = 'text/javascript';s.charset = 'UTF-8';s.async = true;
-      s.src = ('https:' == window.location.protocol ? 'https' : 'http')+'://addtocalendar.com/atc/1.5/atc.min.js';
-      var h = d[g]('body')[0];h.appendChild(s); }})();
-</script>
-
-
-
 </body>
 </html>
