c962ebf1c16d2594bf3d26c30eb2c95b42b5b56f
ursinewalrus <joshkerxhallik@gmail.com>
2017-10-30 19:08:27 -0500

    using acct num

--- www/app/application/controllers/util/housekeeper.php
+++ www/app/application/controllers/util/housekeeper.php
@@ -37,7 +37,7 @@ class Housekeeper extends MY_Controller
                 'currency' => 'usd',
                 'destination' => "rp_1ApIzq2tjBa8SBT2p2kp1IOv",
                 "source" => 'tok_visa'
-            ),array('stripe_account'=>'sk_test_tNbkzCvs0og0cdEgOJbW4NCN'));
+            ),array('stripe_account'=>'acct_1BIm2AAFALFnlpx0'));
             $this->log_echo(print_r($test_charge,true));
             $this->log_echo("Test Charge Complete\n<br>");
 
