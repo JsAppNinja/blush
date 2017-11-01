a5ce04a930b7073aeec0624fb58dbea94faec23d
ursinewalrus <joshkerxhallik@gmail.com>
2017-10-31 21:33:16 -0500

    as array

--- www/app/application/controllers/rest/admin/payables.php
+++ www/app/application/controllers/rest/admin/payables.php
@@ -116,9 +116,9 @@ class Payables extends REST_Controller {
         if ($object->stripe_customer_id) {
             $stripe_recipient = get_stripe_recipient($object->id);
             // $object->account = json_decode($stripe_recipient[0]['active_account']);
-            $object->account = ($stripe_recipient['external_accounts']['data'][0]);
+            $object->account = (array) $stripe_recipient['external_accounts']['data'][0];
 
-            $holder = ($stripe_recipient['external_accounts']['data'][0]->bank_name);
+            $holder = (array)($stripe_recipient['external_accounts']['data'][0]);
             $type = strval(gettype($holder));
         }
         $object->thing = 'here1 '. strval($holder) . $type;
