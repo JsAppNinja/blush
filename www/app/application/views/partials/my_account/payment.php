1b505b5d9320ad6292eef049dba0dee0815337e0
ursinewalrus <joshkerxhallik@gmail.com>
2018-01-25 13:34:15 -0600

    very close to tos update

--- www/app/application/views/partials/my_account/payment.php
+++ www/app/application/views/partials/my_account/payment.php
@@ -5,7 +5,15 @@
     include(APPPATH.'views/partials/my_account/includes/payment-customer.php');
     include(APPPATH.'views/partials/my_account/includes/payment-counselor.php');
     ?>
-
+    <h4>Terms of Service</h4>
+    <div class="row">
+        <div class="col-lg-12">
+            Payment processing services for Coaches on blush are provided by Stripe and are subject to the
+            <a href="https://stripe.com/connect-account/legal">Stripe Connected Account Agreement</a>, which includes the <a href="https://stripe.com/legal">Stripe Terms of Service</a> (collectively, the “Stripe Services Agreement”). By agreeing to this agreement or continuing to operate as a Coach on blush, you agree to be bound by the Stripe Services Agreement, as the same may be modified by Stripe from time to time. As a condition of blush enabling payment processing services through Stripe, you agree to provide blush accurate and complete information about you and your business, and you authorize blush to share it and transaction information related to your use of the payment processing services provided by Stripe.</p>
+        </div>
+        <input id="TosAgree" type="checkbox">
+        <label for="TosAgree">I Agree</label>
+    </div>
     <h4>Location Settings</h4>
 
     <div class="row">
